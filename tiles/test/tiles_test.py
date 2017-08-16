4dcb230ad0ebeb24aeca6f45e7cdf6dc754dde18
Giulio Rossetti <giulio.rossetti@gmail.com>
2017-08-16 14:09:39 +0200

    Update py2 - py3

--- tiles/test/tiles_test.py
+++ tiles/test/tiles_test.py
@@ -9,7 +9,7 @@ class TilesTestCase(unittest.TestCase):
 
     def test_eTILES(self):
         os.makedirs("eres")
-        et = t.eTILES(filename="sample_net_etiles.tsv", obs=1, path="eres")
+        et = t.eTILES(filename="./sample_net_etiles.tsv", obs=1, path="eres")
         et.execute()
 
         count = 0
@@ -22,7 +22,7 @@ class TilesTestCase(unittest.TestCase):
     def test_TILES(self):
 
         os.makedirs("res")
-        et = t.TILES(filename="sample_net_tiles.tsv", obs=1, path="res")
+        et = t.TILES(filename="./sample_net_tiles.tsv", obs=1, path="res")
         et.execute()
 
         count = 0
@@ -33,7 +33,7 @@ class TilesTestCase(unittest.TestCase):
         shutil.rmtree("res")
 
         os.makedirs("res2")
-        et = t.TILES(filename="sample_net_tiles.tsv", obs=1, path="res2", ttl=86400)
+        et = t.TILES(filename="./sample_net_tiles.tsv", obs=1, path="res2", ttl=86400)
         et.execute()
 
         count = 0
